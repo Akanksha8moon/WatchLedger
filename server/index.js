caec7622913bfc88b491d43ff3d4860ae34a074c
himanshu <khatri.hk18@gmail.com>
2025-12-10 16:43:44 +0530

    Revert "feat: Configure Vercel deployment, serve client build from server"
    This reverts commit e97fd7a028b12dc9389a40de58e3614d60d54a2d.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,11 +12,6 @@ const userRoutes = require('./routes/userRoutes');
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 // Routes
 app.use('/api/auth', authRoutes);
